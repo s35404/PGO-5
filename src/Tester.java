382580d970593400c2671344a680c782c77f803d
s35404 <s35404@pjwstk.edu.pl>
2026-04-20 20:34:50 +0200

    Skonczyłem klasy tester i zespol projektowy

--- src/Tester.java
+++ src/Tester.java
@@ -1,5 +1,38 @@
-public class Tester {
+public class Tester extends Pracownik {
     private boolean czyAutomatyzujacy;
     private int liczbaScenariuszy;
+
+public Tester(String idPracownika, String imie, String nazwisko, double stawkaBazowa, boolean czyAutomatyzujacy, int liczbaScenariuszy){
+    super(idPracownika, imie, nazwisko, stawkaBazowa);
+    this.czyAutomatyzujacy = czyAutomatyzujacy;
+    this.liczbaScenariuszy = liczbaScenariuszy;
 }
-public T
+public boolean getczyAutomatyzujacy(){
+    return czyAutomatyzujacy;
+}
+public int getLiczbaScenariuszy(){
+    return liczbaScenariuszy;
+}
+@Override
+    public double obliczKosztMiesieczny(){
+    double koszt = getStawkaBazowa();
+    if(czyAutomatyzujacy == true){
+        koszt +=200.0;
+    }
+    if(liczbaScenariuszy > 30){
+        koszt +=100.0;
+    }
+    return koszt;
+}
+@Override
+    public String przedstawSie() {
+        return "Pracownik: " + pobierzIdPracownika() + "\nImie: " + getImie() + "\nNazwisko: " + getNazwisko() + "\nCzy Automatyzujacy: " + getczyAutomatyzujacy() + "\nLiczba Scenariuszy" + getLiczbaScenariuszy();
+    }
+    public void uruchomRaportTestow() {
+        if (czyAutomatyzujacy == true) {
+            System.out.println("Pracownik " + pobierzIdPracownika() + "ma zautomatyzowany system.");
+        } else {
+        }
+        System.out.println("Pracownik " + pobierzIdPracownika() + " nie ma zautomatyzowanego system.");
+    }
+}
--- src/ZespolProjektowy.java
+++ src/ZespolProjektowy.java
@@ -1,2 +1,48 @@
+import java.util.ArrayList;
+
 public class ZespolProjektowy {
+    private String nazwaProjektu;
+    private ArrayList<Pracownik> pracownicy;
+
+    public ZespolProjektowy(String nazwaProjektu){
+        this.nazwaProjektu = nazwaProjektu;
+        this.pracownicy = new ArrayList<>();
+    }
+    public void dodajPracownika(Pracownik pracownik){
+        this.pracownicy.add(pracownik);
+    }
+    public void wypiszSkladZespolu(){
+        System.out.println("--------Sklad Zespolu--------");
+        for(Pracownik p :  pracownicy){
+            System.out.println(p.przedstawSie());
+            System.out.println("--------------------------");
+        }
+    }
+    public double policzLacznyKoszt(){
+        double laczyKoszt = 0;
+        for(Pracownik p :  pracownicy){
+            laczyKoszt += p.obliczKosztMiesieczny();
+        }
+        return laczyKoszt;
+    }
+    public Pracownik znajdzPoId(String idPojazdu) {
+        for(Pracownik pracownik : pracownicy){
+            if(pracownik.pobierzIdPracownika().equals(idPojazdu)){
+
+            }
+        }
+        return null;
+    }
+    public void wypiszProgramistow(){
+        System.out.println("--------Programiści--------");
+        for(Pracownik pracownik : pracownicy){
+            if(pracownik instanceof Programista);
+                Programista programista = (Programista) pracownik;
+
+                System.out.println(pracownik.przedstawSie());
+                System.out.println("-----------------------");
+        }
+    }
 }
+
+
